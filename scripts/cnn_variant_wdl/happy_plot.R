df14c1bfbb550094e5a1f1181a1ed7aa599ab7b1
Samuel Friedman <sam@broadinstitute.org>
2018-05-26 07:18:12 -0400

    happy wdl updates

--- scripts/cnn_variant_wdl/happy_plot.R
+++ scripts/cnn_variant_wdl/happy_plot.R
@@ -10,15 +10,17 @@ merged <- Reduce(function(...) merge(..., all=T), dnamed)
 melted <- melt(merged, id.vars=c("Name", "Filter", "Type"))
 
 metrics <- subset(melted, variable%in%c("METRIC.Recall", "METRIC.F1_Score", "METRIC.Precision"))
-ggplot(metrics, aes(x=Name, y=value, fill=Filter)) + 
-  geom_bar(stat="identity", position="dodge") +
+ggplot(metrics, aes(x=Name, y=value, color=Filter)) +
+  geom_point(stat="identity", position = position_jitter(w = 0.06, h = 0)) +
+  geom_text(aes(label=round(value,3)),hjust=-0.3, vjust=0) +
   facet_grid( variable ~ Type, scales="free_y" ) +
   theme(axis.text.x=element_text(angle=30, hjust = 1))
 ggsave(filename = 'metrics.png')
 
 counts <- subset(melted, variable%in%c("TRUTH.TP", "TRUTH.FN", "QUERY.FP"))
-ggplot(counts, aes(x=Name, y=value, fill=Filter)) + 
-  geom_bar(stat="identity", position="dodge") +
+ggplot(counts, aes(x=Name, y=value, color=Filter)) +
+  geom_point(stat="identity", position = position_jitter(w = 0.06, h = 0))  +
+  geom_text(aes(label=value), hjust=-0.3, vjust=0.5) +
   facet_grid( variable ~ Type, scales="free_y" ) +
   theme(axis.text.x=element_text(angle=30, hjust = 1))
 ggsave(filename = 'counts.png')
