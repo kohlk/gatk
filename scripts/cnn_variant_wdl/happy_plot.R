aa770671d70e3d9f54072cd9abb9a745af6a650d
Samuel Friedman <sam@broadinstitute.org>
2018-05-30 17:47:50 -0400

    happy plot updates

--- scripts/cnn_variant_wdl/happy_plot.R
+++ scripts/cnn_variant_wdl/happy_plot.R
@@ -12,7 +12,7 @@ melted <- melt(merged, id.vars=c("Name", "Filter", "Type"))
 metrics <- subset(melted, variable%in%c("METRIC.Recall", "METRIC.F1_Score", "METRIC.Precision"))
 ggplot(metrics, aes(x=Name, y=value, color=Filter)) +
   geom_point(stat="identity", position = position_jitter(w = 0.06, h = 0)) +
-  geom_text(aes(label=round(value,3)),hjust=-0.3, vjust=0) +
+  geom_text(aes(label=ifelse((Filter=="PASS"), round(value,3), "")), color="black", size=2.5, hjust=-0.4, vjust=0.5) +
   facet_grid( variable ~ Type, scales="free_y" ) +
   theme(axis.text.x=element_text(angle=30, hjust = 1))
 ggsave(filename = 'metrics.png')
@@ -20,7 +20,7 @@ ggsave(filename = 'metrics.png')
 counts <- subset(melted, variable%in%c("TRUTH.TP", "TRUTH.FN", "QUERY.FP"))
 ggplot(counts, aes(x=Name, y=value, color=Filter)) +
   geom_point(stat="identity", position = position_jitter(w = 0.06, h = 0))  +
-  geom_text(aes(label=value), hjust=-0.3, vjust=0.5) +
+  geom_text(aes(label=ifelse((Filter=="PASS"), value, "")), color="black", size=2.5, hjust=-0.4, vjust=0.5) +
   facet_grid( variable ~ Type, scales="free_y" ) +
   theme(axis.text.x=element_text(angle=30, hjust = 1))
 ggsave(filename = 'counts.png')
